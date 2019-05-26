96ebe1f19d85cc658a0bdd88ba13acfa7d0763e6
pawan rana <pawan.pvt.ltd@gmail.com>
2019-05-27 01:14:14 +0530

    Updates

---
+++ precache-manifest.d0deb5b538c9268dc8e0edffcf9a9938.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea85cb7393848f2a7a9cdf2fc99b921",
+    "url": "/srijan-technology/index.html"
+  },
+  {
+    "revision": "3bb5b193af127ff10b3d",
+    "url": "/srijan-technology/static/css/2.4806bcc5.chunk.css"
+  },
+  {
+    "revision": "2718c2f2c3177adda620",
+    "url": "/srijan-technology/static/css/3.3d2ea56f.chunk.css"
+  },
+  {
+    "revision": "ed62b81d2b69d952ab27",
+    "url": "/srijan-technology/static/css/4.9e4bf7f4.chunk.css"
+  },
+  {
+    "revision": "b9f3b33985684f5c7ca3",
+    "url": "/srijan-technology/static/css/5.c16d20cf.chunk.css"
+  },
+  {
+    "revision": "90cb60ce5d9f4f129e74",
+    "url": "/srijan-technology/static/css/6.92caeaf3.chunk.css"
+  },
+  {
+    "revision": "54d90decf3d9cd87e736",
+    "url": "/srijan-technology/static/css/main.f4882d0e.chunk.css"
+  },
+  {
+    "revision": "3bb5b193af127ff10b3d",
+    "url": "/srijan-technology/static/js/2.089740b2.chunk.js"
+  },
+  {
+    "revision": "2718c2f2c3177adda620",
+    "url": "/srijan-technology/static/js/3.970d670d.chunk.js"
+  },
+  {
+    "revision": "ed62b81d2b69d952ab27",
+    "url": "/srijan-technology/static/js/4.2caea223.chunk.js"
+  },
+  {
+    "revision": "b9f3b33985684f5c7ca3",
+    "url": "/srijan-technology/static/js/5.5fe1a659.chunk.js"
+  },
+  {
+    "revision": "90cb60ce5d9f4f129e74",
+    "url": "/srijan-technology/static/js/6.494b1183.chunk.js"
+  },
+  {
+    "revision": "54d90decf3d9cd87e736",
+    "url": "/srijan-technology/static/js/main.3fe49462.chunk.js"
+  },
+  {
+    "revision": "69078053223342718dba",
+    "url": "/srijan-technology/static/js/runtime~main.b155b35b.js"
+  },
+  {
+    "revision": "86010bfb491ad2a29cfec4f514966ed2",
+    "url": "/srijan-technology/static/media/headphone.86010bfb.jpg"
+  },
+  {
+    "revision": "01d906942f8f08e888ca6d47e9935ee2",
+    "url": "/srijan-technology/static/media/new-stuff.01d90694.jpg"
+  },
+  {
+    "revision": "97ade669d51eac6bc542a01148b34143",
+    "url": "/srijan-technology/static/media/studio3_decade_tile_600x600.97ade669.png"
+  },
+  {
+    "revision": "1b7e066d961a61e06918521b24c7893c",
+    "url": "/srijan-technology/static/media/triangle-bg.1b7e066d.png"
+  }
+]);
